53e684dbd98305359b577e321899c151f8b45802
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-01-25 22:35:25 +0000

    More TODO.

--- smartmerge/__init__.py
+++ smartmerge/__init__.py
@@ -14,6 +14,15 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+"""Smart three way merging of files.
+
+Plugins can register four different kinds of file-specific handlers:
+
+* install_custom_merger(merger, description=None)
+
+Register a function that can do a three-way merge for a specific file
+
+"""
 
 import importlib
 import os
@@ -41,14 +50,74 @@ def default_line_based_merger(base, this, other):
     return m3.merge_lines()
 
 
-def install_custom_merger(merger, description=None):
+class AbstractPerFileMerger(object):
+    """PerFileMerger objects are used by plugins extending merge.
+
+    See ``smartmerge.plugins.news_merge.news_merge`` for an example concrete
+    class.
+
+    :ivar merger: The Merge3Merger performing the merge.
+    """
+
+    def merge_contents(self, merge_params):
+        """Attempt to merge the contents of a single file.
+
+        :param merge_params: A breezy.merge.MergeFileHookParams
+        :return: A tuple of (status, chunks), where status is one of
+            'not_applicable', 'success', 'conflicted', or 'delete'.  If status
+            is 'success' or 'conflicted', then chunks should be an iterable of
+            strings for the new file contents.
+        """
+        return ('not applicable', None)
+
+
+class PerFileMerger(AbstractPerFileMerger):
+    """Merge individual files when self.file_matches returns True.
+
+    This class is intended to be subclassed.  The file_matches and
+    merge_matching methods should be overridden with concrete implementations.
+    """
+
+    def file_matches(self, params):
+        """Return True if merge_matching should be called on this file.
+
+        Only called with merges of plain files with no clear winner.
+
+        Subclasses must override this.
+        """
+        raise NotImplementedError(self.file_matches)
+
+    def merge_contents(self, params):
+        """Merge the contents of a single file."""
+        # Check whether this custom merge logic should be used.
+        if (
+            # OTHER is a straight winner, rely on default merge.
+            params.winner == 'other' or
+            # THIS and OTHER aren't both files.
+            not params.is_file_merge() or
+            # The filename doesn't match
+                not self.file_matches(params)):
+            return 'not_applicable', None
+        return self.merge_matching(params)
+
+    def merge_matching(self, params):
+        """Merge the contents of a single file that has matched the criteria
+        in PerFileMerger.merge_contents (is a conflict, is a file,
+        self.file_matches is True).
+
+        Subclasses must override this.
+        """
+        raise NotImplementedError(self.merge_matching)
+
+
+def install_custom_merger(merger_kls, description=None):
     """Register a new custom merger.
 
     Args:
-      merger: Merger function
+      merger_kls: Merger class
       description: Optional description
     """
-    _custom_mergers.append((merger, description))
+    _custom_mergers.append((merger_kls, description))
 
 
 def smartmerge(base, this, other):
